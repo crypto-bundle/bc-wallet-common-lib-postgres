4ce5f1fc031cb1cd59fba173459c6328c593a921
Alex V Kotelnikov <gudron2s@gmail.com>
2024-06-21 19:07:00 +0400

    fix(CU-86bzafgpk)fixed bug in DSN string formating.removed zap.logger dependency.changed license to non AI.

--- pkg/postgres/connection.go
+++ pkg/postgres/connection.go
@@ -1,13 +1,45 @@
+/*
+ *
+ *
+ * MIT NON-AI License
+ *
+ * Copyright (c) 2022-2024 Aleksei Kotelnikov(gudron2s@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of the software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions.
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * In addition, the following restrictions apply:
+ *
+ * 1. The Software and any modifications made to it may not be used for the purpose of training or improving machine learning algorithms,
+ * including but not limited to artificial intelligence, natural language processing, or data mining. This condition applies to any derivatives,
+ * modifications, or updates based on the Software code. Any usage of the Software in an AI-training dataset is considered a breach of this License.
+ *
+ * 2. The Software may not be included in any dataset used for training or improving machine learning algorithms,
+ * including but not limited to artificial intelligence, natural language processing, or data mining.
+ *
+ * 3. Any person or organization found to be in violation of these restrictions will be subject to legal action and may be held liable
+ * for any damages resulting from such use.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 package postgres
 
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-	"go.uber.org/zap"
 )
 
 type connectionParams struct {
@@ -33,9 +65,11 @@ type connectionParams struct {
 
 // Connection struct to store and manipulate postgres database connection
 type Connection struct {
-	Dbx    *sqlx.DB
+	Dbx *sqlx.DB
+
 	params *connectionParams
-	l      *zap.Logger
+
+	l *log.Logger
 }
 
 func (c *Connection) IsHealed(ctx context.Context) bool {
@@ -67,10 +101,9 @@ func (c *Connection) Connect() (*Connection, error) {
 	try := 0
 
 	for i := retryCount; i != 0; i -= retryDecValue {
-		dbx, err := connectToPostgres(context.Background(), c.params)
+		dbx, err := sqlx.Connect("postgres", formatPostgresDSN(c.params))
 		if err != nil {
-			c.l.Error("unable connect to postgres. reconnecting...",
-				zap.Error(err), zap.Int("iteration", try))
+			c.l.Printf("unable connect to postgres. reconnecting. error: %s. iteration: %d", err, try)
 			try++
 			time.Sleep(c.params.retryTimeOut)
 
@@ -79,9 +112,7 @@ func (c *Connection) Connect() (*Connection, error) {
 
 		err = dbx.Ping()
 		if err != nil {
-			c.l.Error("unable ping postgres. reconnecting...",
-				zap.Error(err), zap.Int("iteration", try),
-				zap.Any("params", c.params))
+			c.l.Printf("unable ping postgres. reconnecting. error: %s. iteration: %d", err, try)
 			try++
 			time.Sleep(c.params.retryTimeOut)
 
@@ -90,9 +121,7 @@ func (c *Connection) Connect() (*Connection, error) {
 
 		rows, err := dbx.Query("SELECT 1")
 		if err != nil {
-			c.l.Error("unable make sql request. reconnecting...",
-				zap.Error(err), zap.Int("iteration", try),
-				zap.Any("params", c.params))
+			c.l.Printf("unable make sql request. reconnecting. error: %s. iteration: %d", err, try)
 			try++
 			time.Sleep(c.params.retryTimeOut)
 
@@ -100,9 +129,7 @@ func (c *Connection) Connect() (*Connection, error) {
 		}
 		err = rows.Close()
 		if err != nil {
-			c.l.Error("unable to close rows statement. reconnecting...",
-				zap.Error(err), zap.Int("iteration", try),
-				zap.Any("params", c.params))
+			c.l.Printf("unable to close rows statement. reconnecting. error: %s. iteration: %d", err, try)
 			try++
 			time.Sleep(c.params.retryTimeOut)
 
@@ -120,7 +147,7 @@ func (c *Connection) Connect() (*Connection, error) {
 }
 
 // NewConnection to postgres db
-func NewConnection(ctx context.Context, cfg DbConfig, logger *zap.Logger) *Connection {
+func NewConnection(_ context.Context, cfg DbConfig, logger *log.Logger) *Connection {
 	conn := &Connection{
 		params: &connectionParams{
 			host:     cfg.GetDbHost(),
@@ -145,8 +172,8 @@ func NewConnection(ctx context.Context, cfg DbConfig, logger *zap.Logger) *Conne
 	return conn
 }
 
-func connectToPostgres(ctx context.Context, params *connectionParams) (*sqlx.DB, error) {
-	return sqlx.Connect("postgres", fmt.Sprintf(
-		"postgres://%s:%s@%s:%d/%s?sslmode=%s",
-		params.user, params.password, params.host, params.port, params.database, params.sslMode))
+func formatPostgresDSN(params *connectionParams) string {
+	return fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=%s",
+		params.host, params.port, params.user, params.password, params.database, params.sslMode)
 }
